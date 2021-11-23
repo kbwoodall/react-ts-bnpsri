2f454ba36e16e296536aed31c537974e56d0e51b
kbwoodall <kerry.woodall@gmail.com>
2021-11-22 22:15:10 -0800

    1 file modified

--- MyUseEffect.tsx
+++ MyUseEffect.tsx
@@ -9,18 +9,27 @@ export function mydemo() {
     console.log('You clicked ' + count + ' times wow');
   }, []);
 
-  return (
-    <div>
-      <p style={{ color: 'blue' }}>You clicked {count} times again</p>
-      <button onClick={() => setCount(count + 1)}>Click me again</button>
-    </div>
-  );
+  return <div>{'xxx'}</div>;
 
   /*
+
+interface FooProp {
+  name: string;
+  X: number;
+  Y: number;
+}
+declare function AnotherComponent(prop: { name: string });
+function ComponentFoo(prop: FooProp) {
+  return <AnotherComponent name={prop.name} />;
+}
+const Button = (prop: { value: string }, context: { color: string }) => (
+  <button />
+);
+
 test
 <p style={{ color: 'blue' }}>You clicked {count} times again</p>
       <button onClick={() => setCount(count + 1)}>Click me again</button>
 
-
+      const MyComponent: FC = () => { <>...</> }
 */
 }
