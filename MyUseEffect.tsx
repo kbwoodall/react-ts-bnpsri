6dd3cc067a3d6920f9cf08578f83a0445dac0ba9
kbwoodall <kerry.woodall@gmail.com>
2021-11-22 23:35:59 -0800

    1 file modified

--- MyUseEffect.tsx
+++ MyUseEffect.tsx
@@ -27,37 +27,6 @@ export function mydemo() {
     console.log('You clicked ' + count + ' times wow');
   }, []);
 
-  type ChildProps = {
-    name: string;
-    id: number;
-    bio?: string;
-  };
-
-  const Child: FC<ChildProps> = (): ReactElement => {
-    const [clickA, setClickA] = useState(0);
-    const [clickB, setClickB] = useState(0);
-
-    useEffect(() => {
-      if (clickA === 0) {
-        console.log('Component loaded!');
-      } else {
-        console.log('Button A was clicked!');
-      }
-    }, [clickA]);
-
-    return (
-      <div>
-        <p>A Clicks: {clickA}</p>
-        <p>B Clicks: {clickB}</p>
-        <button onClick={() => setClickA(clickA + 1)}>Button A</button>
-        <button onClick={() => setClickB(clickB + 1)}>Button B</button>
-        <p id="click-a"></p>
-      </div>
-    );
-  };
-
-  return <div>{'xxx'}</div>;
-
   /*
 
 interface FooProp {
