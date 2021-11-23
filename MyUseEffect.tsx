ae0974e5c48164e84b6db6b6668eb3496a7cd719
kbwoodall <kerry.woodall@gmail.com>
2021-11-22 18:43:40 -0800

    1 file modified

--- MyUseEffect.tsx
+++ MyUseEffect.tsx
@@ -4,7 +4,7 @@ import { useState, useReducer, useEffect } from 'react';
 export function mydemo() {
   // logic will go here ..
   console.log('this is exportable function in TypeScript. !!');
-
+  const [count, setCount] = useState(0);
   useEffect(() => {
     console.log('You clicked ' + count + ' times');
   }, []);
