5a8e8420045a918a4701c1405e5588afbc826c2d
kbwoodall <kerry.woodall@gmail.com>
2021-11-22 18:56:25 -0800

    1 file modified

--- MyUseEffect.tsx
+++ MyUseEffect.tsx
@@ -6,13 +6,8 @@ export function mydemo() {
   console.log('this is exportable function in TypeScript. !!');
   const [count, setCount] = useState(0);
   useEffect(() => {
-    console.log('You clicked ' + count + ' times');
+    console.log('You clicked ' + count + ' times wow');
   }, []);
 
-  return (
-    <div>
-      <p style={{ color: 'blue' }}>You clicked {count} times again</p>
-      <button onClick={() => setCount(count + 1)}>Click me again</button>
-    </div>
-  );
+  
 }
