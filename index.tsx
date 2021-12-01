777db6a55933be71f7fd8d0197735146e919f677
kbwoodall <kerry.woodall@gmail.com>
2021-11-30 23:04:17 -0800

    1 file modified

--- index.tsx
+++ index.tsx
@@ -36,29 +36,6 @@ const CStyle = {
   width: 800,
   //backgroundColor: 'gray'
 };
-const Flex = (props) => (
-  <div
-    className={props.className}
-    style={{
-      display: props.container ? 'flex' : 'block',
-      justifyContent: props.justifyContent || 'flex-start',
-      flexDirection: props.flexDirection || 'row',
-      flexGrow: props.flexGrow || 0,
-      flexBasis: props.flexBasis || 'auto',
-      flexShrink: props.flexShrink || 1,
-      flexWrap: props.flexWrap || 'nowrap',
-      flex: props.flex || '0 1 auto',
-      alignItems: props.alignItems || 'stretch',
-      margin: props.margin || '0',
-      padding: props.padding || '0',
-      width: props.width || 'auto',
-      height: props.height || 'auto',
-      maxWidth: props.maxWidth || 'none',
-    }}
-  >
-    {props.children}
-  </div>
-);
 
 const App = () => {
   return (
@@ -82,7 +59,7 @@ const App = () => {
         <p style={{ color: 'blue' }}>Hooks useEffect</p>
         {UseEffect()}
       </div>
-      </div>
+    </div>
   );
 };
 
