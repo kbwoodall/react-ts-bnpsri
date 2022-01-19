e0709d97676b63db7ef7a4d79cb0fb9111906a9f
kbwoodall <kerry.woodall@gmail.com>
2022-01-18 20:31:46 -0800

    1 file modified

--- MyContext.tsx
+++ MyContext.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import moment from 'moment';
 export const lang = { en: { stuff: 'Yorba Linda' } };
 export const data = { city: 'Yorba Linda' };
 export const location = { city: 'Anaheim' };
@@ -7,16 +8,22 @@ export const LangContext = React.createContext(data);
 export const DataContext = React.createContext(location);
 
 export const getBlock = () => {
+  function getDate() {
+    const dt = moment().format('MMMM Do YYYY, h:mm:ss a');
+    console.log('in formatDate ' + dt);
+    return dt;
+  }
+
   const currentDate = Date.now();
   const timestamp = new Date().getTime();
   var date = new Date(timestamp).toDateString();
 
   // generate a timestamp
-  var timestamp2 = Number(new Date()); //1479895361931
+  var ts2 = Number(getDate); //1479895361931
 
   const blockdata = [
-    { id: 123, name: 'John', date: date, amt: 100.01 },
-    { id: 345, name: 'jack', date: date, amt: 150.23 },
+    { id: 123, name: 'John', date: getDate(), amt: 100.01 },
+    { id: 345, name: 'jack', date: getDate(), amt: 150.23 },
   ];
   for (var i = 0; i < blockdata.length; i++) {
     console.log('testing ' + blockdata[i].name);
