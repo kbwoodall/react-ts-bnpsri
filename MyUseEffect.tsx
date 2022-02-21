c824ad8e7486fd2b80a262403e5dbf64c1011a6a
kbwoodall <kerry.woodall@gmail.com>
2022-02-21 15:20:56 -0800

    1 file modified

--- MyUseEffect.tsx
+++ MyUseEffect.tsx
@@ -47,7 +47,7 @@ export function myhooks(): JSX.Element | null {
 
       <button
         className="ml-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => alert('ok')}
+        onClick={() => setCount(count + 1)}
       >
         Format now
       </button>
