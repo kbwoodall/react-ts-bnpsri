105e0ca47dd3d75f14658488a2357a4072dd8340
kbwoodall <kerry.woodall@gmail.com>
2022-03-07 06:25:49 -0800

    1 file modified

--- MyUseEffect.tsx
+++ MyUseEffect.tsx
@@ -51,7 +51,6 @@ export function myhooks(): JSX.Element | null {
           </p>
         ))}
       </div>
-      {showText(myObj2)}
 
       <button
         className="ml-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
