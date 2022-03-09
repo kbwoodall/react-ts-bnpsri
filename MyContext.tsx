77a378df1dbe1fd1a9a046b9bed26bc78e1635fd
kbwoodall <kerry.woodall@gmail.com>
2022-03-08 21:37:22 -0800

    1 file modified

--- MyContext.tsx
+++ MyContext.tsx
@@ -83,7 +83,7 @@ export const getBlock = () => {
 
       <button
         className="ml-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => alert('ok')}
+        onClick={() => msg}
       >
         Format now
       </button>
