33632f8a6e407edf831f286cef3e63559c02c874
kbwoodall <kerry.woodall@gmail.com>
2022-03-14 18:02:51 -0700

    1 file modified

--- MyContext.tsx
+++ MyContext.tsx
@@ -85,7 +85,7 @@ export const getBlock = () => {
         className="ml-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={() => setMsg("Formatted to Blockchain more")}
       >
-        Format now
+        Format  First
       </button>
     </div>
   );
